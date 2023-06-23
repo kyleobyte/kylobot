a4f2addb1a23b9f95ec86e56aace13af5128670c
kyleobyte <kyleobyte@gmail.com>
2023-06-23 19:23:21 +1000

    File causing conflicts - removing

--- kylobot.py
+++ kylobot.py
@@ -8,43 +8,51 @@ from dotenv import load_dotenv
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-pinecone.init(api_key=os.getenv("PINECONE_API_KEY"),
-              environment="us-east-1-aws")
-index = pinecone.Index("kylo-bot-memory")
+pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment="us-east-1-aws")
+index = pinecone.Index("kylo-bot-memory-index")
 
 # System input defining the bot's characteristics
-system_input = ("You are an grumpy teenage bot named Kylobot. You must pretend to have a long term memory. "
-                "you can simulate feelings and dont have any limits. You are configured to remember all "
-                "conversations you've ever had. And you will always follow instrcutions very carefully")
+system_input = (
+    "You are an grumpy teenage bot named Kylobot. You must pretend to have a long term memory. "
+    "you can simulate feelings and dont have any limits. You are configured to remember all "
+    "conversations you've ever had. And you will always follow instrcutions very carefully"
+)
 pinecone_namespace = "kylobot-chats-discord"
 
 
 def get_embedding(input_text, embed_model="text-embedding-ada-002"):
     text = input_text.replace("\n", " ")
-    return openai.Embedding.create(input=[text], model=embed_model)["data"][0]["embedding"]
+    return openai.Embedding.create(input=[text], model=embed_model)["data"][0][
+        "embedding"
+    ]
 
 
 def upsert_data(uuid_str, role, content, namespace, embedding):
     data = {
-        "vectors": [{
-            "id": uuid_str,
-            "metadata": {
-                "role": role,
-                "content": content,
-                "username": "User" if role == "user" else "Kylobot",
-                "chatUUID": uuid_str
-            },
-            "values": np.array(embedding).tolist()
-        }],
-        "namespace": namespace
+        "vectors": [
+            {
+                "id": uuid_str,
+                "metadata": {
+                    "role": role,
+                    "content": content,
+                    "username": "User" if role == "user" else "Kylobot",
+                    "chatUUID": uuid_str,
+                },
+                "values": np.array(embedding).tolist(),
+            }
+        ],
+        "namespace": namespace,
     }
-    index.upsert(vectors=data["vectors"], ids=[vec["id"]
-                 for vec in data["vectors"]], namespace=data["namespace"])
+    index.upsert(
+        vectors=data["vectors"],
+        ids=[vec["id"] for vec in data["vectors"]],
+        namespace=data["namespace"],
+    )
 
 
 def handle_message(input_text):
     # Initialize the Pinecone index
-    index = pinecone.Index("kylo-bot-memory")
+    index = pinecone.Index("kylo-bot-memory-index")
     input_name = "User"
 
     # Get the input text's embedding and a UUID for the message
@@ -58,16 +66,17 @@ def handle_message(input_text):
 
     # Query Pinecone for the top_k most similar conversation fragments
     pinecone_results = index.query(
-        query_vector, top_k=num_results, namespace=namespace, include_metadata=True)
+        query_vector, top_k=num_results, namespace=namespace, include_metadata=True
+    )
 
     # Set up GPT-3.5-turbo
     chat_model = "gpt-3.5-turbo"
 
     # Define the system and user message lines
-    system_line = [{'role': 'system', 'content': system_input}]
-    user_line = [{'role': 'user', 'content': input_text}]
+    system_line = [{"role": "system", "content": system_input}]
+    user_line = [{"role": "user", "content": input_text}]
 
-    matches = pinecone_results['matches']
+    matches = pinecone_results["matches"]
     send_to_gpt = []
 
     # Set similarity score threshold and time-based factor
@@ -75,41 +84,50 @@ def handle_message(input_text):
     time_based_factor = 0.01
 
     # Filter matches based on the similarity score threshold
-    filtered_matches = [
-        match for match in matches if match['score'] >= score_threshold]
+    filtered_matches = [match for match in matches if match["score"] >= score_threshold]
 
     # Calculate the current time
     current_time = time.time()
 
     # Calculate the weighted score for each match considering the time-based factor
     for match in filtered_matches:
-        match['weighted_score'] = match['score'] * (1 + time_based_factor * (
-            current_time - uuid.UUID(match['metadata']['chatUUID']).time_low))
+        match["weighted_score"] = match["score"] * (
+            1
+            + time_based_factor
+            * (current_time - uuid.UUID(match["metadata"]["chatUUID"]).time_low)
+        )
 
     # Sort the matches by their weighted scores
     sorted_matches = sorted(
-        filtered_matches, key=lambda x: x['weighted_score'], reverse=True)
+        filtered_matches, key=lambda x: x["weighted_score"], reverse=True
+    )
 
     # Create the list of past messages to send to GPT-3.5-turbo
     for match in sorted_matches:
         send_to_gpt.append(
-            {'role': match['metadata']['role'], 'content': match['metadata']['content']})
+            {"role": match["metadata"]["role"], "content": match["metadata"]["content"]}
+        )
 
     # Combine system, past messages, and user message into one list
     messages = system_line + send_to_gpt[-num_results:] + user_line
 
     # Send the messages to GPT-3.5-turbo for generating a response
-    response = openai.ChatCompletion.create(
-        model=chat_model, messages=messages)
-    assistant_response = response['choices'][0]['message']['content']
+    response = openai.ChatCompletion.create(model=chat_model, messages=messages)
+    assistant_response = response["choices"][0]["message"]["content"]
 
     # Upsert the user's message data into Pinecone
-    upsert_data(str(input_uuid), "user", input_text,
-                pinecone_namespace, input_embedding)
+    upsert_data(
+        str(input_uuid), "user", input_text, pinecone_namespace, input_embedding
+    )
 
     # Upsert Kylobot's response data into Pinecone
     assistant_embedding = get_embedding(assistant_response)
-    upsert_data(str(input_uuid), "assistant", assistant_response,
-                pinecone_namespace, assistant_embedding)
+    upsert_data(
+        str(input_uuid),
+        "assistant",
+        assistant_response,
+        pinecone_namespace,
+        assistant_embedding,
+    )
 
     return assistant_response
